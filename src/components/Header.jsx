acd6a1ac3de6517d1c5ebc18d07d802ee7a71f8c
M0H1K4N <nejer911@gmail.com>
2025-02-15 17:41:07 +0400

    addedFunctionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,60 @@
 import React from "react";
+import { useState } from "react";
 import Logo from "../assets/sneakers_logo.svg";
 import cartImg from "../assets/cart-icon.svg";
-import burgerMenuImg from '../assets/burger_menu_icon.svg'
+import burgerMenuImg from "../assets/burger_menu_icon.svg";
 import guyImg from "../assets/guy-img.png";
+import closeButton from "../assets/close-iocon.svg";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <header>
       <section className="header_sections">
-        <img src={burgerMenuImg} alt="" />
+        {isOpen ? (
+          <img onClick={() => setIsOpen(false)} src={burgerMenuImg} alt="" />
+        ) : (
+          <div className="burgerMenu">
+            <div>
+              <img onClick={() => setIsOpen(true)} src={closeButton} alt="" />
+            </div>
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        )}
+
+        {/* <img src={closeButton} alt="" /> */}
         <img className="logo" src={Logo} alt="" />
       </section>
       <section className="header_sections">
-        <img  src={cartImg} alt="" />
+        <img src={cartImg} alt="" />
         <img className="guy-img" src={guyImg} alt="" />
       </section>
     </header>
+  );
+}
+
+function SideBarComponent() {
+  return (
+    <>
+      <div className="burgerMenu">
+        <div>
+          <img onClick={() => setIsOpen(true)} src={closeButton} alt="" />
+        </div>
+        <ul>
+          <li>Collections</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </>
   );
 }
